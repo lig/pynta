b009b721088cd268ccd3ec9fe72c389b27c24d5a
Serge Matveenko <s@matveenko.ru>
2011-11-11 23:57:32 +0400

    Migrate templates to settings provider factory.

--- src/pynta/templates/base.py
+++ src/pynta/templates/base.py
@@ -1,39 +1,9 @@
-class RendererMixinBase(type):
-
-    def __new__(cls, name, bases, args):
-        from pynta.conf import settings
-
-        renderer_settings_name = args.get('renderer_settings_name')
-
-        if renderer_settings_name:
-            # Base I'm. Just project settings I need.
-            renderer_settings = getattr(settings, renderer_settings_name, {})
-        else:
-            # find base settings name 
-            renderer_bases = filter(
-                lambda base: hasattr(base, 'renderer_settings_name'),
-                bases)
-
-            if renderer_bases:
-                # we have settings
-                renderer_settings_name = getattr(renderer_bases[0],
-                    'renderer_settings_name')
-                renderer_settings = getattr(settings, renderer_settings_name,
-                    {})
-                # update renderer_settings with my args
-                renderer_settings.update(args)
-            else:
-                renderer_settings = {}
-
-        # update args with calculated settings
-        args.update(renderer_settings)
-
-        return type.__new__(cls, name, bases, args)
+from pynta.conf.provider import settings_provider_factory
 
 
 class RendererMixin(object):
 
-    __metaclass__ = RendererMixinBase
+    __metaclass__ = settings_provider_factory('renderer')
 
     renderer_settings_name = ''
 
