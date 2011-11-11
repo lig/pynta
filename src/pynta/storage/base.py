22405140c3fdecc06f61b0c1f51b4239644f3601
Serge Matveenko <s@matveenko.ru>
2011-11-12 00:30:59 +0400

    Add Anydbm storage backend.

--- src/pynta/storage/base.py
+++ src/pynta/storage/base.py
@@ -6,3 +6,31 @@ class StorageMixin(object):
     __metaclass__ = settings_provider_factory('storage')
 
     storage_settings_name = ''
+
+    @property
+    def storage(self):
+        NotImplemented
+
+
+class Anydbm(StorageMixin):
+    storage_settings_name = 'STORAGE_ANYDBM'
+
+    filename = None
+    flag = 'r'
+    mode = 0666
+
+
+    def __init__(self, *args, **kwargs):
+        super(Anydbm, self).__init__(*args, **kwargs)
+        import anydbm
+        self.db = anydbm.open(self.filename, self.flag, self.mode)
+
+
+    @property
+    def storage(self):
+        return self.db
+
+
+    def __del__(self):
+        self.db.close()
+        super(Anydbm, self).__del__()
