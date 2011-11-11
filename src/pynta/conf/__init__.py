bca738bfd6302facdf250dbfc934840382056076
Serge Matveenko <s@matveenko.ru>
2011-11-11 15:54:39 +0400

    Improve settings handling.

--- src/pynta/conf/__init__.py
+++ src/pynta/conf/__init__.py
@@ -1,14 +1,58 @@
 import os, sys
 
-from paste.util.import_string import try_import_module
+from paste.util.import_string import import_module
 
 
 class ConfigurationError(Exception):
     pass
 
 
-sys.path.insert(0, os.path.curdir)
-settings = try_import_module('settings')
+class UnconfiguredSettings(object):
 
-if not settings:
-    raise ConfigurationError('No settings. Are you in the project dir?')
+    def __getattribute__(self, name):
+        raise ConfigurationError('Settings is not configured yet.')
+
+
+class Settings(object):
+
+    _settings = None
+
+
+    def __init__(self, settings_module_name=None):
+        from pynta import conf
+
+        if settings_module_name:
+            self._settings = import_module(settings_module_name)
+        else:
+            sys.path.insert(0, os.path.curdir)
+            self._settings = import_module('settings')
+
+        conf.settings = self
+
+
+    def __nonzero__(self):
+        return bool(self._settings)
+
+
+    def __getattr__(self, name):
+        return getattr(self._settings, name)
+
+
+    def __setattr__(self, name, value):
+
+        if name == '_settings':
+
+            if self._settings:
+                raise NotImplementedError('Settings is already configured.')
+            else:
+                super(Settings, self).__setattr__(name, value)
+
+        else:
+            raise NotImplementedError('You cannot change settings at runtime.')
+
+
+    def __delattr__(self, name):
+        raise NotImplementedError('You cannot change settings at runtime.')
+
+
+settings = UnconfiguredSettings()
