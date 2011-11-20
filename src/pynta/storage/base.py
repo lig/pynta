9b3ac3d3521f7c28cc448d365009d4c0cc3ea1b6
Serge Matveenko <s@matveenko.ru>
2011-11-20 23:37:31 +0400

    Add get/put/delete storage methods.

--- src/pynta/storage/base.py
+++ src/pynta/storage/base.py
@@ -2,11 +2,35 @@ import anydbm
 
 
 class Storage(object):
-
+    """
+    Storage interface class. Defines typical storage properties and methods.
+    """
     settings_name = ''
 
     class settings:
         pass
+
+    def get(self, tag, key):
+        """
+        Returns object stored under tag `tag` and key `key`.
+        Think of tag like of the table or collection.
+        Type of the `object` depends on storage realization. 
+        """
+        return NotImplemented
+
+    def put(self, tag, key, obj):
+        """
+        Stores object `obj` under tag `tag` and key `key`.
+        Returned value depends on storage realization.
+        """
+        return NotImplemented
+
+    def delete(self, tag, key):
+        """
+        Deletes object stored under tag `tag` and key `key`.
+        Returned value depends on storage realization.
+        """
+        return NotImplemented
 
 
 class Anydbm(Storage):
@@ -28,3 +52,19 @@ class Anydbm(Storage):
     def __del__(self):
         self.db.close()
         super(Anydbm, self).__del__()
+
+
+    def get(self, tag, key):
+        return self.db[self._get_object_key(tag, key)]
+
+
+    def put(self, tag, key, obj):
+        self.db[self._get_object_key(tag, key)] = obj
+
+
+    def delete(self, tag, key):
+        del self.db[self._get_object_key(tag, key)]
+
+
+    def _get_object_key(self, tag, key):
+        return '%s+%s' % (tag, key)
