6f3e5ef07e411db5566318fb17d5dbc9a620002e
Serge Matveenko <s@matveenko.ru>
2012-08-01 15:38:28 +0400

    Add Mongoengine Storage test.

--- tests/base.py
+++ tests/base.py
@@ -7,6 +7,7 @@ from test_project.test_app.plaintext_app import PlaintextApp
 from test_project.test_app.mako_app import MakoApp
 from test_project.test_app.mongodb_app import MongodbApp
 from test_project.test_app.mongokit_app import MongokitApp
+from test_project.test_app.mongoengine_app import MongoengineApp
 
 
 class PyntaAppTest(unittest.TestCase):
@@ -33,4 +34,5 @@ suite = unittest.TestSuite([
     PyntaAppTest(app_class=MakoApp, etalon_output='test output\n'),
     PyntaAppTest(app_class=MongodbApp, etalon_output='test output\n'),
     PyntaAppTest(app_class=MongokitApp, etalon_output='test output\n'),
+    PyntaAppTest(app_class=MongoengineApp, etalon_output='test output\n'),
 ])
