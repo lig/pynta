2cfd9fa017ec579bc1cd28ed9bc5f6365d420b88
Serge Matveenko <s@matveenko.ru>
2012-08-01 13:13:17 +0400

    Warn if Mongodb storage support is unavailable.

--- src/pynta/storage/__init__.py
+++ src/pynta/storage/__init__.py
@@ -1,6 +1,11 @@
+from warnings import warn
+
 from base import Anydbm
-from mongodb import Mongodb
+try:
+    from mongodb import Mongodb
+except ImportError:
+    warn('Mongodb storage support disabled.')
 try:
     from mongokit_storage import Mongokit
 except ImportError:
-    print 'Mongokit storage support disabled.'
+    warn('Mongokit storage support disabled.')
