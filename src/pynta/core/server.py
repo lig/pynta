645c88887451e6a2dd89f128c0c912133c6e3bba
Serge Matveenko <s@matveenko.ru>
2012-10-10 18:26:51 +0400

    Be verbose on simple server start and stop.

--- src/pynta/core/server.py
+++ src/pynta/core/server.py
@@ -3,4 +3,8 @@ from wsgiref.simple_server import make_server
 
 def serve(host, port, app):
     server = make_server(host, port, app)
-    server.serve_forever()
+    print('Simple server started on %s:%s.' % (host, port))
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Simple server stopped.')
